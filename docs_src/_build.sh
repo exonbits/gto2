55878790a73a933a436fb388b1720c1b0a6bcd73
João Rafael Almeida <joao.rafael.almeida@ua.pt>
2021-04-23 00:07:24 +0100

    Move docs

--- docs_src/_build.sh
+++ docs_src/_build.sh
@@ -6,7 +6,7 @@ Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
 
-rm -r ../site/book/libs/ ../site/book/* || echo "nothing to remove"
+rm -r ../docs/book/libs/ ../docs/book/* || echo "nothing to remove"
 rm -r _book/book/images || echo "nothing to remove"
-mv _book/* ../site/book/
+mv _book/* ../docs/book/
 rmdir _book
